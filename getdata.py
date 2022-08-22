4166252d08f50bde12710b934e7100145394211d
Cedric0852 <mugishac777@gmail.com>
2022-08-22 10:58:47 +0200

    update 2

--- getdata.py
+++ getdata.py
@@ -2,7 +2,10 @@ import requests
 from requestpstatus import projecttoken
 import streamlit as st
 import pandas as pd
+import os
+from dotenv import load_dotenv
+load_dotenv()
 def getingcsv(csvdata):
-    url= "https://www.parsehub.com/api/v2/projects/"+str(projecttoken(projecttoken))+"/last_ready_run/data?api_key=tKp4tAsQ_yyi&format=csv"   
+    url= "{url}"+str(projecttoken(projecttoken))+"/last_ready_run/data?api_key={api_key}&format=csv"   
     df=pd.read_csv(url,index_col=False) 
     return df
--- requestpstatus.py
+++ requestpstatus.py
@@ -1,23 +1,23 @@
-# import requests
-# import json
-# import streamlit as st
-# ###
-# #making api call 
-# params = {
-#   "api_key": "tKp4tAsQ_yyi",
-#   "offset": "0",
-#   "include_options": "0"
-# }
-# # Requesting project status
-# def requeststatus(data):
-#     r = requests.get('https://www.parsehub.com/api/v2/projects/tV3pTmaTkwwL',params=params)
-#     return r.text
+import requests
+import json
+import streamlit as st
+###
+#making api call 
+params = {
+  "api_key": "{api_key}",
+  "offset": "0",
+  "include_options": "0"
+}
+# Requesting project status
+def requeststatus(data):
+    r = requests.get('{pro_url}',params=params)
+    return r.text
    
-# def data1(data1):
-#     data=requeststatus(requeststatus)
-#     jdat=json.loads(data)
-#     return jdat
-# @st.cache
-# def projecttoken(token):
-#     pt=data1(data1)["token"]
-#     return pt
+def data1(data1):
+    data=requeststatus(requeststatus)
+    jdat=json.loads(data)
+    return jdat
+@st.cache
+def projecttoken(token):
+    pt=data1(data1)["token"]
+    return pt
