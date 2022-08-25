9966469c84b67308e53823c9f4d00783f7b0d0b5
Cedric0852 <mugishac777@gmail.com>
2022-08-25 13:29:01 +0200

    Update Results.py

--- pages/Results.py
+++ pages/Results.py
@@ -4,7 +4,7 @@ from itertools import cycle
 import sys
 import os
 st.header("PREDICTA Results page ")
-filteredImages = ["https://github.com/Cedric0852/predicta/blob/main/reslutsimage/*.jpeg"] # your images here
+filteredImages = ["https://github.com/Cedric0852/predicta/blob/main/reslutsimage/Results.jpeg?raw=true"] # your images here
 caption = ["Results"] # your caption here
 cols = cycle(st.columns(4)) # st.columns here since it is out of beta at the time I'm writing this
 for idx, filteredImage in enumerate(filteredImages):
