0f35c19cce2251ba9b027db4cb257f8028d35d6f
Cedric0852 <mugishac777@gmail.com>
2022-12-12 17:18:50 +0200

    update

--- getdata.py
+++ getdata.py
@@ -2,17 +2,15 @@ import requests
 from requestpstatus import projecttoken
 import streamlit as st
 import pandas as pd
-import os
+import os                                                                                                                                                                                                          
 from dotenv import load_dotenv, find_dotenv
 from dotenv.main import load_dotenv
 from pathlib import Path
 load_dotenv()
 key=os.environ["api_key"]
-data_url=os.environ["data_url"]
-data_url_2=os.environ["data_url_2"]
-
+url1=os.environ["data_url"]
+url2=os.environ["data_url_2"]
 def getingcsv(csvdata):
-    url= data_url+str(projecttoken(projecttoken))+data_url_2+"="+key+"&format=csv"   
+    url= str(url1)+str(projecttoken(projecttoken))+str(url2)+"="+str(key)+"&format=csv"   
     df=pd.read_csv(url,index_col=False) 
     return df
-
